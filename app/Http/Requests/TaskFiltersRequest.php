fdc326f8de3f6814e6f8a2dc4aadc8424c3eceec
Yuvrajsinh Jhala <png625@gmail.com>
2023-05-14 11:18:08 +0530

    Feature: Document added

--- app/Http/Requests/TaskFiltersRequest.php
+++ app/Http/Requests/TaskFiltersRequest.php
@@ -11,10 +11,10 @@ class TaskFiltersRequest extends FormRequest
     /**
      * Determine if the user is authorized to make this request.
      */
-    public function authorize(): bool
+    /*public function authorize(): bool
     {
-        return true;
-    }
+        return false;
+    }*/
 
     /**
      * Get the validation rules that apply to the request.
